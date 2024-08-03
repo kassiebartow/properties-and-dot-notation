d30a8a888dab0adb61f67738337438dae5480907
kassie_s <kassiebartow@gmail.com>
2024-08-03 11:15:59 -0500

    Display property values within strings

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 const topicElement = document.querySelector(".topic");
 const countElement = document.querySelector(".count");
+
+const learning = {};
+
+learning.topic = "JS";
+learning.learningGoals = ["Build JS Programs.", "Learn factory functions.", "Complete Intro to APIs", "Complete Guess The Word Game Project"];
+learning.category = "Front-End Development";
+learning.topicImportance = "High";
+console.log(learning)
+
+learning.topic = "JavaScript";
+learning.learningGoals.splice(1, 1);
+
+topicElement.innerText = `I’m learning ${learning.topic}.`;
+topicElement.classList.remove("hide");
+
+countElement.innerText =`I have ${learning.learningGoals.length} learning goals.`;
+countElement.classList.remove("hide");
